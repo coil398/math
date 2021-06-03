944189638b258470a1d9c02ab7e30dc9d76bba43
coil398 <konayuta.sou@gmail.com>
2021-06-04 01:22:49 +0900

    pythagoras

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ mod math;
 
 fn main() {
     println!("sum(10): {}", math::sum(10));
+    println!("triangle(10): {}", math::triangle(10));
+    let p = math::formula::irreducible_pythagoras_numbers(3, 1);
+    match p {
+        Ok(p) => println!("irreducible_pythagoras_numbers(3, 1) = ({})", p),
+        Err(e) => println!("{}", e),
+    }
 }
--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,3 +1,12 @@
+pub mod formula;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum MathError {
+    #[error("Invalid parameters: {0}")]
+    InvalidParameters(String),
+}
+
 pub fn sum(n: u64) -> u64 {
     let mut res = 0;
     for k in 1..=n {
